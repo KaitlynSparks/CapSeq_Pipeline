c46cf853acbe25fb9144a24807b7702c88ee9f00
KaitlynSparks <kaitlyn.sparks11@gmail.com>
2024-02-29 21:50:14 +0000

    Create bed files filtered for results, genes, and transcripts of interest.

--- capseq_pipeline.py
+++ capseq_pipeline.py
@@ -1,96 +1,56 @@
-if __name__ ==  '__main__':
-    
-    #PREAMBLE
-    import pandas as pd
-    from ucsc_genomes_downloader import Genome
-    import pybedtools
-    from pybiomart import Server, Dataset
-    import requests, sys
-    import glob
 
-    '''
-    #GET GENE INTERVALS
-            
-        #create list of gene ensembl IDs from genes_of_interest.txt file
-    with open('data/input/genes_of_interest.txt') as f:
+import pandas as pd
+from ucsc_genomes_downloader import Genome
+import pybedtools
+from pybiomart import Server, Dataset
+import requests, sys
+
+def gene_filter():
+
+    with open('input/genes_of_interest.txt') as f:
         lines = f.read().splitlines()
     gene_ensembl_ID_list = lines
     
-        #specify info to get from query
+    #specify info to get from query
     query_attributes=['chromosome_name', 
-                      'start_position',
-                      'end_position']
+                    'start_position',
+                    'end_position']
     
-        #specify what to filter query on
+    #specify what to filter query on
     query_filters={'transcript_is_canonical': True,
-            'link_ensembl_gene_id': gene_ensembl_ID_list}
+                   'link_ensembl_gene_id': gene_ensembl_ID_list}
     
-        #set what database to use
+    #set what database to use
     ensembl_dataset = Dataset(name='hsapiens_gene_ensembl',
-                      host='http://www.ensembl.org')
+                              host='http://www.ensembl.org')
     
-        #actually submit query
-    gene_region_df = ensembl_dataset.query(attributes=query_attributes, filters = query_filters)
+    #actually submit query
+    gene_region_df = ensembl_dataset.query(attributes=query_attributes,
+                                           filters = query_filters)
         
-        #add chr before chromosome number
+    #add chr before chromosome number
     def chr(x):
         return 'chr' + str(x)
     gene_region_df['Chromosome/scaffold name'] = gene_region_df['Chromosome/scaffold name'].apply(chr) 
 
-        #create mask bedfile from gene regions
+    #create mask bedfile from gene regions
     gene_region_list = gene_region_df.values.tolist()
     joint_bedtool = pybedtools.BedTool(' '.join(map(str,gene_region_list[0])), from_string=True)
     for i in range(1,len(gene_region_list)):
         single_bedtool = pybedtools.BedTool(' '.join(map(str,gene_region_list[i])), from_string=True)
         joint_bedtool = joint_bedtool.cat(single_bedtool, force_truncate=True)
         
-        #sort and save mask bed file
-    joint_bedtool = joint_bedtool.sort(genome="hg38")    
-    joint_bedtool.saveas('data/temp_files/masking_bedfile.bed')
-    '''
+    #sort and save mask bed file
+    gene_masking_bedtool = joint_bedtool.sort(genome="hg38")
+    gene_masking_bedtool.saveas('temp_files/gene_masking_bedfile.bed')
+    return gene_masking_bedtool
 
-    '''
-    #MASK RESULTS FOR GENES OF INTEREST
-
-        #if masking file saved and above code commented out, run this:
-    
-    masking_bedtool = pybedtools.BedTool('data/temp_files/masking_bedfile.bed')
-
-        #otherwise, run:
-    
-    #masking_bedfile = joint_bedtool
-
-        #then run:
-    with open('data/input/CaptureSEQ_results_of_interest.txt') as f:
-        lines = f.read().splitlines()
-    capseq_results_bedfile_list = lines
-
-    headers = ['chrom', 
-               'chromStart',
-               'chromEnd',
-               'name',
-               'score',
-               'strand',
-               'thickStart',
-               'thickEnd',
-               'itemRgb',
-               'blockCount',
-               'blockSizes',
-               'blockStarts']
-    
-    for results_bedfile in capseq_results_bedfile_list:
-        results_bedtool = pybedtools.BedTool('data/input/CaptureSEQ_results/' + results_bedfile)
-        results_bedtool = results_bedtool.sort(genome="hg38")
-        masked_bedtool = masking_bedtool.intersect(results_bedtool, F = 1.0, wb=True)
-        masked_bedtool.saveas('data/temp_files/masked_' + results_bedfile)
-    '''
-
-    #FILTER MASKED RESULTS FOR TRANSCRIPTS OF INTEREST
-    
-        #get transcript list
-    with open('data/input/transcripts_of_interest.txt') as f:
+def transcript_filter(bed_file):
+    with open('input/transcripts_of_interest.txt') as f:
         lines = f.read().splitlines()
     transcript_ensemble_ID_list = lines
+    transcript_and_gene_masked_bed = bed_file[bed_file.iloc[:,6].str.contains('|'.join(transcript_ensemble_ID_list), na=False, regex=False)]
+    return transcript_and_gene_masked_bed
 
 
 
@@ -98,7 +58,8 @@ if __name__ ==  '__main__':
 
 
 
-    #for gene_id in gene_list:
+
+
 
 """     server = "https://rest.ensembl.org"
     ext = "/xrefs/symbol/homo_sapiens/" + gene_id +"?"
@@ -106,3 +67,20 @@ if __name__ ==  '__main__':
 
     hg38 = Genome(assembly="hg38")
     sequences = hg38.bed_to_sequence(transcripts) """
+
+
+#perhaps useful in future
+'''
+headers = ['chrom', 
+            'chromStart',
+            'chromEnd',
+            'name',
+            'score',
+            'strand',
+            'thickStart',
+            'thickEnd',
+            'itemRgb',
+            'blockCount',
+            'blockSizes',
+            'blockStarts']
+'''
--- run.py
+++ run.py
@@ -1,4 +1,44 @@
-import main
+if __name__ ==  '__main__':
+    import capseq_pipeline
+    import glob
+    import pybedtools
+    import pandas as pd
+
+    filter_for_results = True
+    filter_for_genes = True
+    filter_for_transcripts = True
+    clear_temp = True
+
+    if filter_for_results:
+        with open('input/CaptureSEQ_results_of_interest.txt') as f:
+            lines = f.read().splitlines()
+        results_bedfile_name_list = lines
+    else:
+        results_bedfile_name_list = glob.glob('data/*.bed').split('/')[-1]
+
+    for results_bedfile_name in results_bedfile_name_list:
+        results_bedtool = pybedtools.BedTool('data/CaptureSEQ_results/' + results_bedfile_name)
+        gene_masked_bedfile_name = 'temp_files/gene_masked_' + results_bedfile_name
+        
+        results_bedtool = results_bedtool.sort(genome="hg38")
+        if filter_for_genes:
+            gene_masking_bedtool = capseq_pipeline.gene_filter()
+            results_bedtool = gene_masking_bedtool.intersect(results_bedtool, F = 1.0, wb=True)
+        results_bedtool.saveas(gene_masked_bedfile_name)    
+        
+        gene_masked_bed = pd.read_csv(gene_masked_bedfile_name, sep = '\t')
+        transcript_and_gene_masked_bed = gene_masked_bed
+        if filter_for_transcripts:
+            transcript_and_gene_masked_bed = capseq_pipeline.transcript_filter(transcript_and_gene_masked_bed)
+        
+        transcript_and_gene_masked_bed.to_csv('temp_files/transcript_and_' + gene_masked_bedfile_name.split('/')[-1],sep='/t')
+
+    
+    
+
+    
+    
+
 
 
 
